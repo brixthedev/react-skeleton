3e5d886a334c41d9094ee22cc431ca100343fca2
brixthedev <you@example.com>
2020-12-15 22:41:40 +0800

    commit on 'master'

---
+++ src/skeletons/SkeletonArticle.js
@@ -0,0 +1,20 @@
+import SkeletonElement from "./SkeletonElement"
+import "./skeleton.css"
+import Shimmer from "./Shimmer"
+
+const SkeletonArticle = ({ theme }) => {
+    const themeClass = theme || "light"
+
+    return (
+        <div className={`skeleton-wrapper ${themeClass}`}>
+            <div className="skeleton-article">
+                <SkeletonElement type="title" />
+                <SkeletonElement type="text" />
+                <SkeletonElement type="text" />
+            </div>
+            <Shimmer />
+        </div>
+    )
+}
+
+export default SkeletonArticle
